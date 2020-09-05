e84527d7606bc33b54e5a68b311158b3c9dd79ae
Luis Vega <vegaluisjose@gmail.com>
2020-09-04 21:39:47 -0700

    init

---
+++ regression.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -eo pipefail
+
+test_counter=0
+test_fail=0
+
+test_function(){
+   test_counter=`expr $test_counter + 1`
+   if echo $1 | grep -q $2 ; then
+       echo -e "\033[01;32mPass"
+   else
+       test_fail=`expr $test_fail + 1`
+       echo -e "\033[01;31mFail"
+   fi
+}
+
+test_function "aaa" "e"
+test_function "aaa" "a"
+
+echo -ne "\033[01;0m"
+echo -e "Number of tests:$test_counter"
+echo -e "Number of failed tests:$test_fail"
+
+test $test_fail -eq 0
